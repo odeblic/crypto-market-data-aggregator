72d239d89c07c20b58b9e99f8f0df939d5b77a3c
Olivier de Blic <odeblic@workstation.home.arpa>
2026-09-07 23:00:13 +0800

    Scope down the utilization of Boost to optimize builds

--- cmake/boost.cmake
+++ cmake/boost.cmake
@@ -1,5 +1,7 @@
 # Add Boost as a dependency for Websockets
 
+set(BOOST_INCLUDE_LIBRARIES lockfree beast system)
+
 FetchContent_Declare(
     Boost
     URL https://github.com/boostorg/boost/releases/download/boost-1.84.0/boost-1.84.0.tar.gz

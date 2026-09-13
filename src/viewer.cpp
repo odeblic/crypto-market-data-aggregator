948ff16a494bd9d43e4eb8ebacf054ddadca0960
Olivier de Blic <odeblic@workstation.home.arpa>
2026-09-13 17:34:20 +0800

    Load configuration files for the viewer

--- src/viewer.cpp
+++ src/viewer.cpp
@@ -1,3 +1,6 @@
+#include "cfg/Loader.hpp"
+#include "cfg/ViewerConfiguration.hpp"
+//#include "core/Arguments.hpp"
 #include "core/MarketDataLogger.hpp"
 #include "core/Utils.hpp"
 #include "ws/WebsocketClient.hpp"
@@ -6,9 +9,14 @@
 
 int main(int argc, char * argv[])
 {
-    if (argc != 2)
+    if (argc != 3)
     {
-        std::cerr << "An exchange must be specified:\n"
+        std::cerr << "Please specify an exchange and a configuration file.\n\n";
+        std::cerr << "Usage:\n\t"
+                  << argv[0] << " CONFIG_FILE EXCHANGE_NAME\n\n";
+        std::cerr << "Example:\n\t"
+                  << argv[0] << " config/viewer.json bitmex\n\n";
+        std::cerr << "List of available exchanges:\n"
                   << "- Binance\n"
                   << "- BitMEX\n"
                   << "- Bybit\n"
@@ -20,10 +28,14 @@ int main(int argc, char * argv[])
         return 1;
     }
 
-    auto const exchange = std::string(argv[1]);
+    //auto arguments = Arguments{argc, argv};
+    //auto path = arguments.getConfigFilePath();
+    auto const path = argv[1];
+    auto const exchangeName = argv[2];
+    auto const exchange = fromString<Exchange>(exchangeName);
+    auto const config = loadConfigFromFile<ViewerConfiguration>(path);
     MarketDataLogger logger;
     WebsocketClient client{logger};
-    client.connect(fromString<Exchange>(exchange));
+    client.connect(exchange, config.exchanges.at(exchangeName));
     client.run();
-    return 0;
 }

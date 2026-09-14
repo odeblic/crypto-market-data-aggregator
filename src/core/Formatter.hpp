fe02865055b8fcb7484c76098c08d80d97e0ddb1
Olivier de Blic <odeblic@workstation.home.arpa>
2026-09-15 00:32:40 +0800

    Improve price and quantity formatting in the console

--- src/core/Formatter.hpp
+++ src/core/Formatter.hpp
@@ -8,14 +8,23 @@
 #include <sstream>
 #include <string>
 
+static inline auto toString(double value, bool precise) -> std::string
+{
+    size_t const precision = (precise ? 6 : 2);
+    size_t const length = 7 + precision;
+    std::ostringstream buffer;
+    buffer << std::setw(length) << std::fixed << std::setprecision(precision) << value;
+    return buffer.str();
+}
+
 static inline auto toString(MarketUpdate const& update) -> std::string
 {
     std::stringstream buffer;
     buffer << "ticker: \033[35m" << toString(update.ticker) << "\033[0m "
-            << "side: " << (update.side == Side::ASK ? "\033[31mASK\033[0m " : "\033[32mBID\033[0m ")
-            << "price: \033[33m" << std::fixed << std::setprecision(2) << update.price << "\033[0m "
-            << "quantity: \033[33m" << std::defaultfloat << update.quantity << "\033[0m "
-            << "source: \033[34m" << toString(update.exchange) << "\033[0m\n";
+           << "side: " << (update.side == Side::ASK ? "\033[31mASK\033[0m " : "\033[32mBID\033[0m ")
+           << "price: \033[33m" << toString(update.price, false) << "\033[0m "
+           << "quantity: \033[33m" << toString(update.quantity, true) << "\033[0m "
+           << "source: \033[34m" << toString(update.exchange) << "\033[0m\n";
     return buffer.str();
 }
 
@@ -23,7 +32,7 @@ static inline auto toString(Book const& book, size_t const depth = 10) -> std::s
 {
     std::stringstream buffer;
     size_t level = 0;
-    buffer << "\n+--------------------+--------------------+\n";
+    buffer << "\n+--------------------------+--------------------------+\n";
 
     while ((level < book.ask.size() || level < book.bid.size()) && level < depth)
     {
@@ -33,16 +42,14 @@ static inline auto toString(Book const& book, size_t const depth = 10) -> std::s
         {
             auto const& quote = book.bid[level];
             buffer << "\033[32m"
-                    << std::setfill(' ') << std::setw(7) << std::fixed << std::setprecision(2)
-                    << quote.quantity
-                    << "\033[0m * \033[32m"
-                    << std::setfill(' ') << std::setw(7) << std::fixed << std::setprecision(2)
-                    << quote.price
+                    << toString(quote.quantity, true)
+                    << "\033[0m  \033[32m"
+                    << toString(quote.price, false)
                     << "\033[0m";
         }
         else
         {
-            buffer << "                  ";
+            buffer << "                        ";
         }
 
         buffer << " | ";
@@ -51,22 +58,20 @@ static inline auto toString(Book const& book, size_t const depth = 10) -> std::s
         {
             auto const& quote = book.ask[level];
             buffer << "\033[31m"
-                    << std::setfill(' ') << std::setw(7) << std::fixed << std::setprecision(2)
-                    << quote.price
-                    << "\033[0m * \033[31m"
-                    << std::setfill(' ') << std::setw(7) << std::fixed << std::setprecision(2)
-                    << quote.quantity
+                    << toString(quote.price, false)
+                    << "\033[0m  \033[31m"
+                    << toString(quote.quantity, true)
                     << "\033[0m";
         }
         else
         {
-            buffer << "                  ";
+            buffer << "                        ";
         }
 
         buffer << " |\n";
         level++;
     }
 
-    buffer << "+--------------------+--------------------+\n";
+    buffer << "+--------------------------+--------------------------+\n";
     return buffer.str();
 }
--- src/rpc/MarketDataFormatter.hpp
+++ src/rpc/MarketDataFormatter.hpp
@@ -14,14 +14,15 @@ static inline auto toString(marketdata::Snapshot const& snapshot) -> std::string
 
     for (auto const& ask : snapshot.asks())
     {
-        buffer << "ASK " << ask.price() << " " << ask.quantity() << '\n';
+        buffer << "ASK  price:" << toString(ask.price(), false) << "  quantity:" << toString(ask.quantity(), true) << '\n';
     }
 
     for (auto const& bid : snapshot.bids())
     {
-        buffer << "BID " << bid.price() << " " << bid.quantity() << '\n';
+        buffer << "BID  price:" << toString(bid.price(), false) << "  quantity:" << toString(bid.quantity(), true) << '\n';
     }
 
+    buffer << "end of snapshot for symbol: " << snapshot.symbol() << '\n';
     return buffer.str();
 }
 
@@ -29,9 +30,10 @@ static inline auto toString(marketdata::Update const& update) -> std::string
 {
     std::stringstream buffer;
     buffer << "update for symbol: " << update.symbol()
-            << " | " << (update.side() == marketdata::Side::BID ? "BID" : "ASK")
-            << " | price: " << update.price()
-            << " | quantity: " << update.quantity() 
-            << '\n';
+           << '\n'
+           << (update.side() == marketdata::Side::BID ? "BID" : "ASK")
+           << "  price:" << toString(update.price(), false)
+           << "  quantity:" << toString(update.quantity(), true)
+           << '\n';
     return buffer.str();
 }

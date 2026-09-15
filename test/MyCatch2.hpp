fe10d6d594d79695df2f9ed4b9dc558fa83148aa
Olivier de Blic <odeblic@workstation.home.arpa>
2026-09-15 17:17:27 +0800

    Fallback to Catch2 v2 if v3 cannot be found

--- test/MyCatch2.hpp
+++ test/MyCatch2.hpp
@@ -1,8 +1,16 @@
 #pragma once
 
-#include <catch2/catch.hpp>
-//#include <catch2/catch_test_macros.hpp>
-//#include <catch2/matchers/catch_matchers_floating_point.hpp>
+#if __has_include(<catch2/catch_test_macros.hpp>)
+    // Catch2 v3+ (Modern, fast compilation header)
+    #include <catch2/catch_test_macros.hpp>
+    #include <catch2/matchers/catch_matchers_floating_point.hpp>
+    //#include <catch2/matchers/catch_matchers_all.hpp>
+#elif __has_include(<catch2/catch.hpp>)
+    // Catch2 v2 (Legacy monolithic header)
+    #include <catch2/catch.hpp>
+#else
+    #error "Catch2 header not found!"
+#endif
 
 #define PRICE_TOLERANCE 0.001
 #define PRICE_MATCHER(PRICE) Catch::Matchers::WithinAbs((PRICE), PRICE_TOLERANCE)

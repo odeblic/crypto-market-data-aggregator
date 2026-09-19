13c897090252374806da79e4d48885254018d5fa
Olivier de Blic <odeblic@workstation.home.arpa>
2026-09-19 21:54:49 +0800

    Use the right stream for Binance and parse the right messages

--- src/cex/ExchangeSessionBinance.hpp
+++ src/cex/ExchangeSessionBinance.hpp
@@ -22,12 +22,17 @@ struct ExchangeSessionBinance : ClientSession
 
     virtual auto checkMessage(nlohmann::json const& msg) const -> bool override
     {
-        if (!msg.contains("asks"))
+        if (!msg.contains("e") || msg["e"] != "depthUpdate")
         {
             return false;
         }
 
-        if (!msg.contains("bids"))
+        if (!msg.contains("a"))
+        {
+            return false;
+        }
+
+        if (!msg.contains("b"))
         {
             return false;
         }
@@ -38,37 +43,38 @@ struct ExchangeSessionBinance : ClientSession
     virtual void processMessage(nlohmann::json const& msg) override
     {
         auto const exchange = Exchange::BINANCE;
+        auto const ticker = fromString<Ticker>(msg["s"].get<std::string>());
 
-        auto const parseAndPublish = [this](nlohmann::json const & entries, Side const side)
+        auto const parseAndPublish = [this, &ticker](nlohmann::json const& entries, Side const side)
         {
             if (entries.is_array() == false)
             {
                 return;
             }
 
-            for (auto const & entry : entries)
+            for (auto const& entry : entries)
             {
                 if (entry.is_array() && entry.size() >= 2)
                 {
                     MarketUpdate marketUpdate{};
                     marketUpdate.price = std::stod(entry[0].get<std::string>());
                     marketUpdate.quantity = std::stod(entry[1].get<std::string>());
+                    marketUpdate.ticker = ticker;
                     marketUpdate.side = side;
                     marketUpdate.exchange = exchange;
-
                     publish(marketUpdate);
                 }
             }
         };
 
-        if (msg.contains("asks"))
+        if (msg.contains("a"))
         {
-            parseAndPublish(msg["asks"], Side::ASK);
+            parseAndPublish(msg["a"], Side::ASK);
         }
 
-        if (msg.contains("bids"))
+        if (msg.contains("b"))
         {
-            parseAndPublish(msg["bids"], Side::BID);
+            parseAndPublish(msg["b"], Side::BID);
         }
     }
 };
